9d5a6c2344dcfc2a1afcaba123eaa456c3ddb330
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2021-07-30 03:02:34 -0500

    Update startPingTestbed.sh

--- Tests/startPingTestbed.sh
+++ Tests/startPingTestbed.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-ping -i 0.2 -c 20 192.168.0.2 |  ts '[%Y-%m-%d %H:%M:%.S]' | tee pingResultsEPC.txt
+ping -i 0.2 -c 20 192.168.0.1 |  ts '[%Y-%m-%d %H:%M:%.S]' | tee pingResultsEPC.txt
