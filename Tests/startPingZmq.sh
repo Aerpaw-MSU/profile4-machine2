20a172e9d3a81fb4e6cc5049aceefa028ecb21e3
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2021-07-30 03:04:04 -0500

    Update startPingZmq.sh

--- Tests/startPingZmq.sh
+++ Tests/startPingZmq.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-ping -i 0.2 -c 20 172.16.0.2 |  ts '[%Y-%m-%d %H:%M:%.S]' | tee pingResultsEPC.txt
+ping -i 0.2 -c 20 172.16.0.1 |  ts '[%Y-%m-%d %H:%M:%.S]' | tee pingResultsEPC.txt
