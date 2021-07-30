8de242c3d898e4c6ec12a70be51b3ff809a7b317
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2021-07-30 03:02:01 -0500

    Update startMachine1.sh

--- startMachine1.sh
+++ startMachine1.sh
@@ -15,14 +15,14 @@ if [ $MODE == "TESTBED" ]
   mknod /dev/net/tun c 10 200
   ip tuntap add mode tun srs_spgw_sgi	
   screen -S run -dm bash -c "./ofdm_rx_b210_tuntap_with_tx.py | ts '[%Y-%m-%d %H:%M:%.S]' > testbed.log" 
-  screen -S setip -dm bash -c "ifconfig tap2 192.168.0.1"
+  screen -S setip -dm bash -c "ifconfig tap2 192.168.0.2"
   
 elif [ $MODE == "ZMQ" ]
 then
   
   
   screen -S run -dm bash -c "./ofdm_zmq.py | ts '[%Y-%m-%d %H:%M:%.S]' > zmq.log"
-  screen -S setip -dm bash -c "ifconfig tap2 172.17.0.1"
+  screen -S setip -dm bash -c "ifconfig tap2 172.17.0.2"
  
 
  
@@ -34,7 +34,7 @@ then
  screen -S docker -dm bash -c "docker-compose up"
  sleep 10
 # screen -S ping -dm bash -c "
- docker exec -it tests_node_1_1 ping -w 60 172.17.0.3 | ts '[%Y-%m-%d %H:%M:%.S]' > zchemping1.log
+ docker exec -it tests_node_1_1 ping -w 60 172.17.0.2 | ts '[%Y-%m-%d %H:%M:%.S]' > zchemping1.log
  sleep 70
  pkill screen
 #screen -S copy -dm bash -c "docker cp tests_node_1_1:/home/scripts/zchemping.log /"
