892370916b0e2c85998efe26b17408949226d936
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2021-07-30 03:04:29 -0500

    Update startIperfZmq.sh

--- Tests/startIperfZmq.sh
+++ Tests/startIperfZmq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -S iperf3 -dm bash -c "iperf3 -s | ts '[%Y-%m-%d %H:%M:%.S]' > iperf3.log"
+screen -S iperf3 -dm bash -c "iperf3 -c 172.17.0.1 | ts '[%Y-%m-%d %H:%M:%.S]' > iperf3.log"
